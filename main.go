612a549475ddf2898c03c48844bd06251981c925
cryptic-rebel <63493132+cryptic-rebel@users.noreply.github.com>
2021-01-17 15:06:18 +0900

    Update main.go

--- main.go
+++ main.go
@@ -5,18 +5,18 @@ package main
 // int
 // bridge_int_func(intFunc f)
 // {
-//	return f();
+//      return f();
 // }
 //
 // int fortytwo()
 // {
-//	return 42;
+//      return 42;
 // }
-import "c"
+import "C"
 import "fmt"
 
 func main() {
-	f := C.intFunc(C.fortytwo)
-	fmt.Println(int(C.bridge_int_func(f)))
-	// Output: 42
+    f := C.intFunc(C.fortytwo)
+    fmt.Println(int(C.bridge_int_func(f)))
+    // Output: 42
 }
